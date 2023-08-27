ac9f691f8b1e0de40b0aa3aba571e7636f341dab
Julian <juliandbf14@gmail.com>
2023-08-26 22:15:27 -0500

    Corrección Ejercicio#2

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 
 namespace Ejercicio2.Program
 {
@@ -11,39 +10,41 @@ namespace Ejercicio2.Program
             int b = 0;
             int respuestaOperacion = 0;
             int opcion = 0;
-            
-            while (opcion == 0 || opcion >4)
-            {
-                Console.WriteLine("Escribe el primer número:");
-                a = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Escribe el segundo número:");
-                a = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Ingresa la opción que quieres realizar:");
-                Console.WriteLine("1. Sumar (+).\n2. Resta(-)\n3. Multiplicar(*)\n4. División" );
 
-                switch (opcion)
-                {
-                    case 1: 
-                        respuestaOperacion = a + b;
-                        Console.WriteLine($"La suma de los numeros {a} y {b} es: {respuestaOperacion}");
-                        break;
-                    case 2: 
-                        respuestaOperacion = a - b;
-                        Console.WriteLine($"La resta de los numeros {a} y {b} es: {respuestaOperacion}");
-                        break;
-                    case 3: 
-                        respuestaOperacion = a * b;
-                        Console.WriteLine($"El producto de los numeros {a} y {b} es: {respuestaOperacion}");
-                        break;
-                    case 4: 
-                        respuestaOperacion = a / b;
-                        Console.WriteLine($"La división de los numeros {a} y {b} es: {respuestaOperacion}");
-                        break;
-                    default:
+            Console.WriteLine("Escribe el primer número:");
+            a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe el segundo número:");
+            b = Int32.Parse(Console.ReadLine());
+
+            while (opcion == 0 || opcion > 4)
+            {
+                Console.WriteLine("Ingresa la opción que quieres realizar:");
+                Console.WriteLine("1. Sumar (+).\n2. Resta(-)\n3. Multiplicar(*)\n4. División");
+                Console.WriteLine("Escoge una de las opciones, escribiendo su número correspondiente:");
+                opcion = Int32.Parse(Console.ReadLine());
+            }
+
+            switch (opcion)
+            {
+                case 1:
+                    respuestaOperacion = a + b;
+                    Console.WriteLine($"La suma de los numeros {a} y {b} es: {respuestaOperacion}");
+                    break;
+                case 2:
+                    respuestaOperacion = a - b;
+                    Console.WriteLine($"La resta de los numeros {a} y {b} es: {respuestaOperacion}");
+                    break;
+                case 3:
+                    respuestaOperacion = a * b;
+                    Console.WriteLine($"El producto de los numeros {a} y {b} es: {respuestaOperacion}");
+                    break;
+                case 4:
+                    respuestaOperacion = a / b;
+                    Console.WriteLine($"La división de los numeros {a} y {b} es: {respuestaOperacion}");
+                    break;
+                default:
                     Console.WriteLine("Esta operación no esta disponible");
                     break;
-                }
-
             }
         }
     }
